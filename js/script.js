0aac9ea93dbf3a0c658d34235683b35d8ecb0e54
Augusto Shoiti <augustoshoiti@gmail.com>
2022-10-27 20:49:09 -0300

    Testes e ajustes

--- js/script.js
+++ js/script.js
@@ -45,10 +45,12 @@ const renderPokemon = async (pokemon) => {
     pokemonAltura.innerHTML = 'Altura: ???';
 
     const data = await fetchPokemon(pokemon);
-    
+
     if (data) {
         let cal1 = (data['weight'] * 10) / 100 + 'kg';
         let cal2 = (data['height'] * 10) / 100 + 'm';
+
+        searchPokemon = data.id;
 
         input.value = '';
         pokemonImage.style.display = 'block';
@@ -67,17 +69,176 @@ const renderPokemon = async (pokemon) => {
         pokemonSp_Atk.innerHTML = 'Ataque esp: ' + data['stats']['3']['base_stat'];
         pokemonSp_Def.innerHTML = 'Defesa esp: ' + data['stats']['4']['base_stat'];
 
+       
         pokemonTipo1.innerHTML = data['types']['0']['type']["name"];
         pokemonTipo2.innerHTML = data['types']['1']['type']["name"];
 
-        
-        searchPokemon = data.id;
+        // let tipo2 = data['types']['1']['type']["name"];
+        // let tipo1 = data['types']['0']['type']["name"];
+        // switch (tipo1) {
+        //     case "bug":
+        //         pokemonTipo1.style.background = "#90C12D";
+        //         pokemonTipo1.style.border = "1px solid black";
+        //         break;
+        //     case "dark":
+        //         pokemonTipo1.style.background = "#5A5366";
+        //         pokemonTipo1.style.border = "1px solid black";
+        //         break;
+        //     case "dragon":
+        //         pokemonTipo1.style.background = "#0969C1";
+        //         pokemonTipo1.style.border = "1px solid black";
+        //         break;
+        //     case "electric":
+        //         pokemonTipo1.style.background = "#F4D23C";
+        //         pokemonTipo1.style.border = "1px solid black";
+        //         break;
+        //     case "fairy":
+        //         pokemonTipo1.style.background = "#ED8DE6";
+        //         pokemonTipo1.style.border = "1px solid black";
+        //         break;
+        //     case "fighting":
+        //         pokemonTipo1.style.background = "#CF3E68";
+        //         pokemonTipo1.style.border = "1px solid black";
+        //         break;
+        //     case "fire":
+        //         pokemonTipo1.style.background = "#FF9C54";
+        //         pokemonTipo1.style.border = "1px solid black";
+        //         break;
+        //     case "flying":
+        //         pokemonTipo1.style.background = "#91ABDE";
+        //         pokemonTipo1.style.border = "1px solid black";
+        //         break;
+        //     case "ghost":
+        //         pokemonTipo1.style.background = "#9AB0EE";
+        //         pokemonTipo1.style.border = "1px solid black";
+        //         break;
+        //     case "grass":
+        //         pokemonTipo1.style.background = "#60B954";
+        //         pokemonTipo1.style.border = "1px solid black";
+        //         break;
+        //     case "ground":
+        //         pokemonTipo1.style.background = "#D97745";
+        //         pokemonTipo1.style.border = "1px solid black";
+        //         break;
+        //     case "ice":
+        //         pokemonTipo1.style.background = "#73CEC0";
+        //         pokemonTipo1.style.border = "1px solid black";
+        //         break;
+        //     case "normal":
+        //         pokemonTipo1.style.background = "#9099A1";
+        //         pokemonTipo1.style.border = "1px solid black";
+        //         break;
+        //     case "poison":
+        //         pokemonTipo1.style.background = "#A666C7";
+        //         pokemonTipo1.style.border = "1px solid black";
+        //         break;
+        //     case "psychic":
+        //         pokemonTipo1.style.background = "#F86E74";
+        //         pokemonTipo1.style.border = "1px solid black";
+        //         break;
+        //     case "rock":
+        //         pokemonTipo1.style.background = "#C7B78B";
+        //         pokemonTipo1.style.border = "1px solid black";
+        //         break;
+        //     case "steel":
+        //         pokemonTipo1.style.background = "#4F889B";
+        //         pokemonTipo1.style.border = "1px solid black";
+        //         break;
+        //     case "water":
+        //         pokemonTipo1.style.background = "#5398D7";
+        //         pokemonTipo1.style.border = "1px solid black";
+        //         break;
+        //     default:
+        //         pokemonTipo1.style.background = "#FFFFFF";
+        //         pokemonTipo1.style.border = "1px solid black";
+        // }
+        // switch (tipo2) {
+        //     case "bug":
+        //         pokemonTipo2.style.background = "#90C12D";
+        //         pokemonTipo2.style.border = "1px solid black";
+        //         break;
+        //     case "dark":
+        //         pokemonTipo2.style.background = "#5A5366";
+        //         pokemonTipo2.style.border = "1px solid black";
+        //         break;
+        //     case "dragon":
+        //         pokemonTipo2.style.background = "#0969C1";
+        //         pokemonTipo2.style.border = "1px solid black";
+        //         break;
+        //     case "electric":
+        //         pokemonTipo2.style.background = "#F4D23C";
+        //         pokemonTipo2.style.border = "1px solid black";
+        //         break;
+        //     case "fairy":
+        //         pokemonTipo2.style.background = "#ED8DE6";
+        //         pokemonTipo2.style.border = "1px solid black";
+        //         break;
+        //     case "fighting":
+        //         pokemonTipo2.style.background = "#CF3E68";
+        //         pokemonTipo2.style.border = "1px solid black";
+        //         break;
+        //     case "fire":
+        //         pokemonTipo2.style.background = "#FF9C54";
+        //         pokemonTipo2.style.border = "1px solid black";
+        //         break;
+        //     case "flying":
+        //         pokemonTipo2.style.background = "#91ABDE";
+        //         pokemonTipo2.style.border = "1px solid black";
+        //         break;
+        //     case "ghost":
+        //         pokemonTipo2.style.background = "#9AB0EE";
+        //         pokemonTipo2.style.border = "1px solid black";
+        //         break;
+        //     case "grass":
+        //         pokemonTipo2.style.background = "#60B954";
+        //         pokemonTipo2.style.border = "1px solid black";
+        //         break;
+        //     case "ground":
+        //         pokemonTipo2.style.background = "#D97745";
+        //         pokemonTipo2.style.border = "1px solid black";
+        //         break;
+        //     case "ice":
+        //         pokemonTipo2.style.background = "#73CEC0";
+        //         pokemonTipo2.style.border = "1px solid black";
+        //         break;
+        //     case "normal":
+        //         pokemonTipo2.style.background = "#9099A1";
+        //         pokemonTipo2.style.border = "1px solid black";
+        //         break;
+        //     case "poison":
+        //         pokemonTipo2.style.background = "#A666C7";
+        //         pokemonTipo2.style.border = "1px solid black";
+        //         break;
+        //     case "psychic":
+        //         pokemonTipo2.style.background = "#F86E74";
+        //         pokemonTipo2.style.border = "1px solid black";
+        //         break;
+        //     case "rock":
+        //         pokemonTipo2.style.background = "#C7B78B";
+        //         pokemonTipo2.style.border = "1px solid black";
+        //         break;
+        //     case "steel":
+        //         pokemonTipo2.style.background = "#4F889B";
+        //         pokemonTipo2.style.border = "1px solid black";
+        //         break;
+        //     case "water":
+        //         pokemonTipo2.style.background = "#5398D7";
+        //         pokemonTipo2.style.border = "1px solid black";
+        //         break;
+        //     default:
+        //         pokemonTipo2.style.background = "#FFFFFF";
+        // }
+       
     } else {
         input.value = '';
         pokemonImage.src = './image/erro.png';
         pokemonName.innerHTML = 'Not found'
         pokemonNumber.innerHTML = '😰'
         pokemonTipo1.innerHTML = '???';
+        // pokemonTipo1.style.background = "";
+        // pokemonTipo1.style.border = "";
+        // pokemonTipo2.style.background = "";
+        // pokemonTipo2.style.border = "";
         pokemonTipo2.innerHTML = '???';
         pokemonHp.innerHTML = 'Hp: ???';
         pokemonAtk.innerHTML = 'Ataque: ???';
